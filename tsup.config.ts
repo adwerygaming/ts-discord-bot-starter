2f6fb2f6e8c15dffc459792a098f4965016ca4a6
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2026-02-16 21:56:36 +0700

    build(tsup): Set output target to ES2023
    - build(tsup): Configure compilation target for ES2023

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     entry: ['src/**/*.ts', "!src/**/node_modules/**"],
     outDir: 'dist',
     format: ['esm'],
+    target: "es2023",
     sourcemap: false,
     dts: false,
     clean: true,
