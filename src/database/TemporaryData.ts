174dae3a0bc7ef3362420e838162a505fb8659c5
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2026-02-17 08:47:41 +0700

    refactor(temp-data): Adjust functions; add lint script
    - refactor(temp-data): Extract cache operations into individual functions
    - refactor(temp-data): Export individual functions instead of object methods
    - chore(scripts): Add 'lint' command to run ESLint

--- src/database/TemporaryData.ts
+++ src/database/TemporaryData.ts
@@ -14,44 +14,49 @@ interface DeleteDataProp {
     key: string
 }
 
+/**
+ * Generate a random unique key using UUID v7
+ * @returns string - generated unique key
+ */
+function GenerateRandomKey(): string {
+    return uuidv7()
+}
+
+/**
+ * Set data in cache
+ * @param data Data to set in cache
+ * @param key Key to identify the data in cache. Use GenerateRandomKey() to get a random unique key. 
+ * @returns 
+ */
+function SetData<T>({ data, key }: SetDataProp<T>): true {
+    cache.set(key, data)
+    return true
+}
+
+/**
+ * Get data from cache
+ * @param key Key to identify the data in cache 
+ * @returns T | undefined - data if found, undefined if not found
+ */
+async function GetData<T>({ key }: GetDataProp): Promise<T | undefined> {
+    const res: T | undefined = await cache.get(key) as T | undefined
+    return res
+}
+
+/**
+* Delete data from cache
+* @param key Key to identify the data in cache
+* @returns T | undefined - data if found, undefined if not found
+*/
+function DeleteData<T>({ key }: DeleteDataProp): T | undefined {
+    const data: T | undefined = cache.get(key) as T | undefined
+    cache.delete(key)
+    return data
+}
+
 export default {
-    /**
-     * Generate a random unique key using UUID v7
-     * @returns string - generated unique key
-     */
-    GenerateRandomKey(): string {
-        return uuidv7()
-    },
-
-    /**
-     * Set data in cache
-     * @param data Data to set in cache
-     * @param key Key to identify the data in cache. Use GenerateRandomKey() to get a random unique key. 
-     * @returns 
-     */
-    SetData<T>({ data, key }: SetDataProp<T>): true {
-        cache.set(key, data)
-        return true
-    },
-
-    /**
-     * Get data from cache
-     * @param key Key to identify the data in cache 
-     * @returns T | undefined - data if found, undefined if not found
-     */
-    async GetData<T>({ key }: GetDataProp): Promise<T | undefined> {
-        const res: T | undefined = await cache.get(key) as T | undefined
-        return res
-    },
-
-    /**
-        * Delete data from cache
-        * @param key Key to identify the data in cache
-        * @returns T | undefined - data if found, undefined if not found
-        */
-    DeleteData<T>({ key }: DeleteDataProp): T | undefined {
-        const data: T | undefined = cache.get(key) as T | undefined
-        cache.delete(key)
-        return data
-    }
+    GenerateRandomKey,
+    SetData,
+    GetData,
+    DeleteData
 }
